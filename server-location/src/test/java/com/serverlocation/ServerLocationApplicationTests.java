21de187e4ef7a5e4fe2c0ac0bf71023b209b6b12
Amir <61427423+Amir-UL-Islam@users.noreply.github.com>
2024-03-30 18:41:13 +0600

    Server Discovery Using Eureka

---
+++ server-location/src/test/java/com/serverlocation/ServerLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
